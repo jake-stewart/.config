cd0c13108504c4d41c7956909c9750745e639511
jake-stewart <83528263+jake-stewart@users.noreply.github.com>
2021-05-06 15:41:26 +0800

    Update flash_window.sh

--- bspwm/flash_window.sh
+++ bspwm/flash_window.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
+# flashes the active window a bunch of times so you can find it
+
 bspc config focused_border_color "#28ABB4"
+
 sleep 0.1
 bspc config focused_border_color "#4f5769"
+
 sleep 0.1
 bspc config focused_border_color "#28ABB4"
+
 sleep 0.1
 bspc config focused_border_color "#4f5769"
+
 sleep 0.1
 bspc config focused_border_color "#28ABB4"
+
 sleep 0.1
 bspc config focused_border_color "#4f5769"
