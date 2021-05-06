c7ff24e49552c7926723d08526a817751fec11c0
jake-stewart <83528263+jake-stewart@users.noreply.github.com>
2021-05-06 15:43:15 +0800

    Update start_monocle.sh

--- bspwm/start_monocle.sh
+++ bspwm/start_monocle.sh
@@ -1,10 +1,12 @@
-desktops=$(bspc query -D --names)
-for desktop in $desktops; do
-    echo 0 > "$HOME/.cache/bspwm_monocle/desktop_$desktop"
-done
+#!/bin/bash
 
+# bspwm doesn't do per-monitor monocle padding
+# this is a hacky solution
 
 # ~/.cache/monocle_desktop_n
 #       contains 1 or 0 depending on if desktop is in monocle mode
 
-
+desktops=$(bspc query -D --names)
+for desktop in $desktops; do
+    echo 0 > "$HOME/.cache/bspwm_monocle/desktop_$desktop"
+done
